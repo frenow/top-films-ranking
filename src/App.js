1120afff86ce9fa1bc52de173b7b4d496ea7e239
Emerson D <frenow@outlook.com>
2022-08-08 16:35:59 -0300

    add mui framework

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import ReactStars from "react-rating-stars-component";
+import { GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+import { auth } from './services/firebase';
+import IconButton from '@mui/material/IconButton';
+import GoogleIcon from '@mui/icons-material/Google';
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import AdbIcon from '@mui/icons-material/Adb';
+
 import './App.css';
 const API_KEY = 'd416af5d4faee64e25ab001d87aab5c3';
 
 function App() {
+  const [user, setUser] = useState([]);
   const [movies, setMovies] = useState([]);
   const [star, setStar] = useState(0);
+
+
+  function handleGoogleSignin(){
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+    .then((result)=> {
+      setUser(result.user);
+      console.log(result);
+    })    
+    .catch((error)=> {
+      console.log(error);
+    }
+    )
+  }
+
 
   const rating = {
     size: 30,
@@ -29,10 +58,47 @@ function App() {
 
   return (
     <div className="App">
-        <h1>Top Films Ranking</h1>
+
+<AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+        <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            TOP FILMS RANKING
+          </Typography>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Box sx={{ flexGrow: 0 }}>
+              <IconButton>
+                <Avatar alt={user?.displayName} src={user?.photoURL} />
+              </IconButton>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+
+    <IconButton aria-label="Entrar com google" color="primary">
+        <GoogleIcon onClick={handleGoogleSignin} />
+    </IconButton>         
         <ul style={{ listStyle: "none" }}>
         {movies.map(movie => (
-          <li>{movie.title}<ReactStars {...rating} /><img src={"https://image.tmdb.org/t/p/original"+movie.poster_path} alt="poster" width="50" height="60"/></li>
+        <div className="App-list">
+          <div className="App-card">
+              <li key={movie.id}>{movie.title}<ReactStars {...rating} /><img src={"https://image.tmdb.org/t/p/original"+movie.poster_path} alt="poster" width="60" height="70"/></li>
+          </div>
+        </div>
         ))}
         </ul>
     </div>

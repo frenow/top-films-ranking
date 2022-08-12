fa5fbdb585c1270d8279246993c430d3039849e1
Emerson D <frenow@outlook.com>
2022-08-12 15:23:40 -0300

    add detalhe

--- src/App.js
+++ src/App.js
@@ -16,13 +16,38 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import CardMedia from '@mui/material/CardMedia';
+
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
 import './App.css';
 
 function App() {
   const [user, setUser] = useState([]);
   const [movies, setMovies] = useState([]);
+  const [movieDetalhe, setMovieDetalhe] = useState([]);
+
   const [star, setStar] = useState(0);
 
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    fetchMovieDetalhe();
+    setOpen(true);
+  };  
+
+  const handleClose = () => {
+    setOpen(false);
+  };  
+
+  async function fetchMovieDetalhe() {
+    const { data } = await axios.get(`https://api.themoviedb.org/3/movie/616037?api_key=d416af5d4faee64e25ab001d87aab5c3`);
+    console.log(data.results); 
+    setMovieDetalhe(data.results);
+  }
 
   function handleGoogleSignin(){
     const provider = new GoogleAuthProvider();
@@ -69,9 +94,6 @@ function App() {
           </Typography>
           <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
           <Box sx={{ flexGrow: 0 }}>
-              {/*<IconButton>
-                <Avatar alt={user.displayName} src={user.photoURL} />
-              </IconButton>*/}
                <img src={user.photoURL} alt={user.displayName} className="App-avatar" style={{ display: "box" }} />
           </Box>
         </Toolbar>
@@ -97,12 +119,30 @@ function App() {
                 </CardContent>
                 <Rating name="ranking-star" value={star} onChange={(event, newValue) => { setStar(newValue) }} /> 
                 <CardActions>
-                  <Button size="small">Saiba mais</Button>
+                  <Button size="small" onClick={() => handleClickOpen()}>Saiba mais</Button>
                 </CardActions>
-              </Card>  
+              </Card>
+              <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+              >
+                <DialogTitle id="alert-dialog-title">
+                  {movie.title}
+                </DialogTitle>
+                <DialogContent>
+                  <DialogContentText id="alert-dialog-description">
+                    {movieDetalhe}
+                  </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleClose} autoFocus>Ok</Button>
+                </DialogActions>
+              </Dialog>                
               </div>
         ))}
-      </div>
+      </div>    
     </>
   );
 }
